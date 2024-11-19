4439fece7cbe3b63d7aa78176e9316870d6140f1
Aliaksei Lakhmakou <alex.hooke@web.de>
2024-11-19 17:51:51 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,7 +37,42 @@ const App = () => {
       <Routes>
         <Route
           path="/"
-          element={<main>{/* Home-Seite Code unverändert */}</main>}
+          element={<main>{
+            <div>
+                            <Container maxWidth="sm">
+                                <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+                                    Kleiderschrank-Projekt
+                                </Typography>
+                                <Typography variant='h5' align='center' color='textSecondary' paragraph>
+                                    Das ist eine Probe Seite für das Projekt in SOPRA im WS 24/25
+                                </Typography>
+                                <div>
+                                    <Grid container spacing={2} justifyContent="center">
+                                        <Grid item>
+                                            <Button 
+                                                component={Link} 
+                                                to="/login" 
+                                                variant="contained" 
+                                                color='primary'
+                                            >
+                                                Sign-In
+                                            </Button>
+                                        </Grid>
+                                        <Grid item>
+                                            <Button 
+                                                component={Link} 
+                                                to="/register" 
+                                                variant="outlined" 
+                                                color='primary'
+                                            >
+                                                Sign-Up
+                                            </Button>
+                                        </Grid>
+                                    </Grid>
+                                </div>
+                            </Container>
+                        </div>
+          }</main>}
         />
         <Route path="/login" element={<Login onLogin={handleLogin} />} />
         <Route path="/register" element={<Register />} />
@@ -48,7 +83,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
