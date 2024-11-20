94f6171dda15d8af0d3c212a1184d81cf9672021
MorenoContieri <mc076@hdm-stuttgart.de>
2024-11-20 14:46:06 +0100

    Create Style.py

---
+++ src/server/bo/Style.py
@@ -0,0 +1,51 @@
+class Style:
+    def __init__(self):
+        self.__style_id = 0
+        self.__features = ""
+        self.__constraints = []
+        self.__kleidungstypen = []
+
+    
+    def get_style_id(self):
+        return self.__style_id
+
+    def set_style_id(self, style_id: int):
+        self.__style_id = style_id
+
+    
+    def get_features(self):
+        return self.__features
+
+    def set_features(self, features: str):
+        self.__features = features
+
+    
+    def get_constraints(self):
+        return self.__constraints
+
+    def set_constraints(self, constraints: list):
+        self.__constraints = constraints
+
+   
+    def get_kleidungstypen(self):
+        return self.__kleidungstypen
+
+    def set_kleidungstypen(self, kleidungstypen: list):
+        self.__kleidungstypen = kleidungstypen
+
+    def __str__(self):
+        """Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz."""
+        return (
+            f"Style ID: {self.__style_id}, Features: {self.__features}, "
+            f"Constraints: {self.__constraints}, Kleidungstypen: {self.__kleidungstypen}"
+        )
+
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        """Umwandeln eines Python dict() in ein Style()."""
+        obj = Style()
+        obj.set_style_id(dictionary.get("style_id", 0))
+        obj.set_features(dictionary.get("features", ""))
+        obj.set_constraints(dictionary.get("constraints", []))
+        obj.set_kleidungstypen(dictionary.get("kleidungstypen", []))
+        return obj
