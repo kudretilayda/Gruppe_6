6c660568a3357e520f5d9d1a894ba1cf6ec92fc7
Verena Böhner <103314386+VerenaBoehner@users.noreply.github.com>
2024-11-25 13:19:52 +0100

    Create class Administration

--- src/server/Admin.py
+++ src/server/Admin.py
@@ -11,3 +11,28 @@ from .bo.Outfit import Outfit
 from .bo.Style import Style
 from .bo.UnaryConstraint import UnaryConstraint
 from .bo.User import User
+
+
+
+
+
+class Administration(object):
+    """Diese Klasse aggregiert nahezu sämtliche Applikationslogik (engl. Business Logic).
+    Sie ist wie eine Spinne, die sämtliche Zusammenhänge in ihrem Netz (in unserem
+    Fall die Daten der Applikation) überblickt und für einen geordneten Ablauf und
+    dauerhafte Konsistenz der Daten und Abläufe sorgt.
+    Die Applikationslogik findet sich in den Methoden dieser Klasse. Jede dieser
+    Methoden kann als *Transaction Script* bezeichnet werden. Dieser Name
+    lässt schon vermuten, dass hier analog zu Datenbanktransaktion pro
+    Transaktion gleiche mehrere Teilaktionen durchgeführt werden, die das System
+    von einem konsistenten Zustand in einen anderen, auch wieder konsistenten
+    Zustand überführen. Wenn dies zwischenzeitig scheitern sollte, dann ist das
+    jeweilige Transaction Script dafür verwantwortlich, eine Fehlerbehandlung
+    durchzuführen.
+    Diese Klasse steht mit einer Reihe weiterer Datentypen in Verbindung. Diese
+    sind:
+    - die Klassen BusinessObject und deren Subklassen,
+    - die Mapper-Klassen für den DB-Zugriff."""
+
+    def __init__(self):
+        pass
