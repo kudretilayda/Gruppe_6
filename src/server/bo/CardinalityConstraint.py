5921a4d6df8c07a73dc7e7eab7e5d122419e6ddd
Verena Böhner <103314386+VerenaBoehner@users.noreply.github.com>
2024-11-28 22:12:54 +0100

    Update BO Constraints

--- src/server/bo/CardinalityConstraint.py
+++ src/server/bo/CardinalityConstraint.py
@@ -4,19 +4,16 @@ from src.server.bo.Constraint import Constraint
 class CardinalityConstraint(Constraint):
     """Realisierung einer CardinalityConstraint.
 
-    Eine CardinalityConstraint besitzt zwei Attribute (obj1_attribute, obj2_attribute),
-    zwei Werte (obj1_value, obj2_value), eine Mindestanzahl (_min_count) und eine Höchstanzahl (_max_count),
+    Eine CardinalityConstraint besitzt zwei Objekte (obj1, obj2), 
+    eine Mindestanzahl (_min_count) und eine Höchstanzahl (_max_count),
     sowie eine Auswertungsfunktion, die die Bedingung bewertet.
     """
-    def __init__(self, _min_count=0, _max_count=0, _obj1_attribute="",
-                 _obj1_value="", _obj2_attribute="", _obj2_value=""):
+    def __init__(self, _min_count=0, _max_count=0, _obj1=None, _obj2=None):
         super().__init__()
-        self._min_count = _min_count            # Minimale Kardinalität
-        self._max_count = _max_count            # Maximale Kardinalität
-        self._obj1_attribute = _obj1_attribute  # Das Attribut des ersten Objekts
-        self._obj1_value = _obj1_value          # Der Wert des Attributs des ersten Objekts
-        self._obj2_attribute = _obj2_attribute  # Das Attribut des zweiten Objekts
-        self._obj2_value = _obj2_value          # Der Wert des Attributs des zweiten Objekts
+        self._min_count = _min_count  # Minimale Kardinalität
+        self._max_count = _max_count  # Maximale Kardinalität
+        self._obj1 = _obj1            # Erstes Objekt
+        self._obj2 = _obj2            # Zweites Objekt
 
     def get_min_count(self):
         return self._min_count
@@ -30,29 +27,17 @@ class CardinalityConstraint(Constraint):
     def set_max_count(self, value):
         self._max_count = value
 
-    def get_obj1_attribute(self):
-        return self._obj1_attribute
+    def get_obj1(self):
+        return self._obj1
 
-    def set_obj1_attribute(self, value):
-        self._obj1_attribute = value
+    def set_obj1(self, value):
+        self._obj1 = value
 
-    def get_obj1_value(self):
-        return self._obj1_value
+    def get_obj2(self):
+        return self._obj2
 
-    def set_obj1_value(self, value):
-        self._obj1_value = value
-
-    def get_obj2_attribute(self):
-        return self._obj2_attribute
-
-    def set_obj2_attribute(self, value):
-        self._obj2_attribute = value
-
-    def get_obj2_value(self):
-        return self._obj2_value
-
-    def set_obj2_value(self, value):
-        self._obj2_value = value
+    def set_obj2(self, value):
+        self._obj2 = value
 
     def auswerten(self, collection):
         if self._min_count <= self._max_count:
@@ -63,9 +48,8 @@ class CardinalityConstraint(Constraint):
     def __str__(self):
         """Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz."""
         return (
-            f"CardinalityConstraint: (Min: {self._min_count}, Max: {self._max_count},  "
-            f"Obj1: ({self._obj1_attribute}, {self._obj1_value}), "
-            f"Obj2: ({self._obj2_attribute}, {self._obj2_value}))"
+            f"CardinalityConstraint: (Min: {self._min_count}, Max: {self._max_count}, "
+            f"Obj1: {self._obj1}, Obj2: {self._obj2})"
         )
 
     @classmethod
@@ -75,8 +59,6 @@ class CardinalityConstraint(Constraint):
         return cls(
             _min_count=dictionary.get("min_count", 0),
             _max_count=dictionary.get("max_count", 0),
-            _obj1_attribute=dictionary.get("obj1_attribute", ""),
-            _obj1_value=dictionary.get("obj1_value", ""),
-            _obj2_attribute=dictionary.get("obj2_attribute", ""),
-            _obj2_value=dictionary.get("obj2_value", "")
+            _obj1=dictionary.get("obj1", None),
+            _obj2=dictionary.get("obj2", None)
         )
--- src/server/bo/MutexConstraint.py
+++ src/server/bo/MutexConstraint.py
@@ -4,73 +4,63 @@ from src.server.bo.Constraint import Constraint
 class MutexConstraint(Constraint):
     """Realisierung einer MutexConstraint.
 
-    Eine MutexConstraint besitzt zwei Attribute (obj1_attribute, obj2_attribute),
-    zwei Werte (obj1_value, obj2_value) und eine Auswertungsfunktion, die die Bedingung
-    und Implikation bewertet.
+    Eine MutexConstraint besitzt zwei Objekte (`obj1` und `obj2`), die jeweils Attribute und Werte beinhalten.
     """
-    def __init__(self):
+    def __init__(self, obj1=None, obj2=None):
         super().__init__()
-        self._obj1_attribute = ""
-        self._obj1_value = ""
-        self._obj2_attribute = ""
-        self._obj2_value = ""
+        self._obj1 = obj1  # Das erste Objekt
+        self._obj2 = obj2  # Das zweite Objekt
 
-    def get_obj1_attribute(self):
-        return self._obj1_attribute
+    def get_obj1(self):
+        """Auslesen des ersten Objekts."""
+        return self._obj1
 
-    def set_obj1_attribute(self, value):
+    def set_obj1(self, value):
+        """Setzen des ersten Objekts."""
+        self._obj1 = value
 
-        self._obj1_attribute = value
+    def get_obj2(self):
+        """Auslesen des zweiten Objekts."""
+        return self._obj2
 
-    def get_obj1_value(self):
-        return self._obj1_value
+    def set_obj2(self, value):
+        """Setzen des zweiten Objekts."""
+        self._obj2 = value
 
-    def set_obj1_value(self, value):
-        self._obj1_value = value
-
-    def get_obj2_attribute(self):
-        return self._obj2_attribute
-
-    def set_obj2_attribute(self, value):
-        self._obj2_attribute = value
-
-    def get_obj2_value(self):
-        return self._obj2_value
-
-    def set_obj2_value(self, value):
-        self._obj2_value = value
-
-    def auswerten(self, obj):
+    def auswertung(self):
         """Evaluierung der MutexConstraint.
 
-        Überprüft, ob die angegebenen Attribute und Werte nicht gleichzeitig erfüllt sind.
+        Überprüft, ob die angegebenen Objekte und deren Werte nicht gleichzeitig erfüllt sind.
         Gibt True zurück, wenn die Constraint eingehalten wird, andernfalls False.
         """
-        if self._obj1_attribute and self._obj1_value and self._obj2_attribute and self._obj2_value:
-            # Beispielhafte Logik, hier könnte weitere spezifische Logik hinzukommen
-            return not (self._obj1_value == self._obj2_value)
+        if self._obj1 is not None and self._obj2 is not None:
+            # Beispielhafte Logik: Wenn die Attribute und Werte der beiden Objekte erfüllt sind,
+            # dürfen sie nicht denselben Wert haben (Mutex-Prinzip).
+            if self._obj1.get_value() == self._obj2.get_value():  # Beispielhafte Logik für Mutex
+                return False
         return True
 
     def __str__(self):
         """Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz.
 
-        Diese besteht aus der ID der Superklasse ergänzt durch die beiden Objekte
-        und deren Werte.
+        Diese besteht aus den beiden Objekten und deren Attributen und Werten.
         """
-        return "MutexConstraint: Obj1: ({}, {}), Obj2: ({}, {})".format(
-            self._obj1_attribute, self._obj1_value, self._obj2_attribute, self._obj2_value
+        return (
+            f"MutexConstraint: "
+            f"Obj1: ({self._obj1.get_attribute()}, {self._obj1.get_value()}), "
+            f"Obj2: ({self._obj2.get_attribute()}, {self._obj2.get_value()})"
         )
-    # {}, self.get_id(),
 
     @staticmethod
     def from_dict(dictionary=None):
-
+        """Umwandeln eines Python dict() in eine MutexConstraint."""
         if dictionary is None:
             dictionary = dict()
-        obj = MutexConstraint()
-#        obj.set_id(dictionary["id"])  # Eigentlich Teil von BusinessObject!
-        obj.set_obj1_attribute(dictionary["obj1_attribute"])
-        obj.set_obj1_value(dictionary["obj1_value"])
-        obj.set_obj2_attribute(dictionary["obj2_attribute"])
-        obj.set_obj2_value(dictionary["obj2_value"])
-        return obj
+        
+        # Wir nehmen an, dass die Objekte aus einem Dictionary erstellt werden müssen,
+        # die Attribute obj1 und obj2 müssen dabei Instanzen von anderen Klassen sein, 
+        # die mit get_attribute() und get_value() arbeiten.
+        obj1 = dictionary.get("obj1", None)  # Annahme, dass obj1 ein Objekt mit den Methoden get_attribute() und get_value() ist
+        obj2 = dictionary.get("obj2", None)  # Annahme, dass obj2 ein Objekt mit den Methoden get_attribute() und get_value() ist
+        
+        return MutexConstraint(obj1=obj1, obj2=obj2)
