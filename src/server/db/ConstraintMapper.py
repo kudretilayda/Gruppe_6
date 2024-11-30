85cfe5bc0a05895b34cbbf4e4cdeaf6bb8207ad7
Aliaksei Lakhmakou <alex.hooke@web.de>
2024-11-30 11:35:57 +0100

    Update Constraint Mapper
    Hinzufügen der Unary + Binary Constraint 'find_all' abfrage.

--- src/server/db/ConstraintMapper.py
+++ src/server/db/ConstraintMapper.py
@@ -38,6 +38,10 @@ class ConstraintMapper:
                 constraints.append(MutexConstraint(result[0], result[2], result[3]))
             elif constraint_type == 'cardinality':
                 constraints.append(CardinalityConstraint(result[0], result[2], result[3], result[4]))
+            elif constraint_type == 'unary':
+                constraints.append(UnaryConstraint(result[0], result[2], result[3]))
+            elif constraint_type == 'binary':
+                constraints.append(BinaryConstraint(result[0], result[2], result[3], result[4]))
         return constraints
 
     def save(self, constraint):
