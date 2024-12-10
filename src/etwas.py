238d726e10d2462ffeed2ad698b5256429f3d65d
Marsel_vadlja <167113362+MarselProg2@users.noreply.github.com>
2024-12-10 12:07:18 +0100

    Update.py
    Konzept um die Outfits mit einer for schleife bringen

--- src/etwas.py
+++ src/etwas.py
@@ -35,3 +35,20 @@ class Constraint:
 
 tshirt1 = Kleidungsstueck('t-shirt 1')
 tshirt1.kleidungstyp = Kleidungstyp('tshirt')
+
+'''Constraints mit einer for schleife durchlaufen
+
+outfits_to_check = [
+    ("Kurze Hose", "Winterjacke"),
+    ("Jogginghose", "Sakko"),
+    ("T-Shirt", "Anzug"),
+    ("Jeans", "T-Shirt")  # Gültiges Outfit
+]
+for outfit in outfits_to_check:
+    constraint = BinaryConstraint(outfit[0], outfit[1], ist_unzulässiges_outfit)
+    try:
+        constraint.auswerten(None)
+        print(f"Outfit {outfit[0]} und {outfit[1]} ist zulässig.")
+    except ValueError as e:
+        print(e)
+'''
