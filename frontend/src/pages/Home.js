d0c14cbca5b9d8539f3869e4c98a25ec3e4591a6
MorenoContieri <mc076@hdm-stuttgart.de>
2024-12-12 16:20:09 +0100

    Update Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,21 @@
+// src/pages/Home.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Willkommen zu deinem digitalen Kleiderschrank</h1>
+      <p>Verwalte deine Kleidung, Styles und Outfits einfach und effizient.</p>
+      <nav>
+        <ul>
+          <li><Link to="/kleiderschrank">Kleiderschrank</Link></li>
+          <li><Link to="/styles">Styles</Link></li>
+          <li><Link to="/outfits">Outfits</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Home;
