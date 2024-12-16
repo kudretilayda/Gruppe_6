79f7e286520f7219564ae2f0fafa44fadff023db
Kudret Kilic <kk179@hdm-stuttgart.de>
2024-12-16 12:51:48 +0100

    Update main1

--- src/main1.py
+++ src/main1.py
@@ -166,3 +166,65 @@ class UserOperations(Resource):
         adm.delete_user(user)
         return '', 200
 
+@wardrobe_ns.route('/user/<int:user_id>/wardrobe')
+@wardrobe_ns.response(500, 'Server-Error')
+@wardrobe_ns.param('user_id', 'ID of the user')
+class UserWardrobeOperations(Resource):
+    @wardrobe_ns.marshal_with(wardrobe)
+    @secured
+    def get(self, user_id):
+        """Get the wardrobe of a user"""
+        adm = Admin()
+        wardrobe = adm.get_wardrobe_by_user(user_id)
+        return wardrobe
+
+    @wardrobe_ns.marshal_with(wardrobe, code=200)
+    @wardrobe_ns.expect(wardrobe)
+    @secured
+    def post(self, user_id):
+        """Create a new wardrobe for a user"""
+        adm = Admin()
+        proposal = Wardrobe.from_dict(api.payload)
+        if proposal is not None:
+            proposal.set_user_id(user_id)
+            wardrobe = adm.create_wardrobe(proposal)
+            return wardrobe, 200
+        else:
+            return '', 500
+
+# API Endpoints for Wardrobe
+
+@wardrobe_ns.route('/wardrobes/<int:wardrobe_id>')
+@wardrobe_ns.response(500, 'Server-Error')
+@wardrobe_ns.param('wardrobe_id', 'ID of the wardrobe')
+class WardrobeOperations(Resource):
+    @wardrobe_ns.marshal_with(wardrobe)
+    @secured
+    def get(self, wardrobe_id):
+        """Get a wardrobe by ID"""
+        adm = Admin()
+        wardrobe = adm.get_wardrobe_by_id(wardrobe_id)
+        return wardrobe
+
+    @wardrobe_ns.marshal_with(wardrobe)
+    @wardrobe_ns.expect(wardrobe, validate=True)
+    @secured
+    def put(self, wardrobe_id):
+        """Update a wardrobe"""
+        adm = Admin()
+        wardrobe = Wardrobe.from_dict(api.payload)
+        if wardrobe is not None:
+            wardrobe.set_id(wardrobe_id)
+            adm.save_wardrobe(wardrobe)
+            return wardrobe, 200
+        else:
+            return '', 500
+
+    @secured
+    def delete(self, wardrobe_id):
+        """Delete a wardrobe"""
+        adm = Admin()
+        wardrobe = adm.get_wardrobe_by_id(wardrobe_id)
+        adm.delete_wardrobe(wardrobe)
+        return '', 200
+
