b197c174330dcb00e98fd023c2985a2a50e662c9
Aliaksei Lakhmakou <alex.hooke@web.de>
2024-12-16 18:48:11 +0100

    Fehlerbehebung

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ from src.server.bo.Wardrobe import Wardrobe
 from src.server.bo.Style import Style
 from src.server.bo.Outfit import Outfit
 from src.server.bo.ClothingItem import ClothingItem
-from src.server.bo.clothingtype import ClothingType
+from src.server.bo.ClothingType import ClothingType
 from src.server.bo.Constraints import ConstraintRule, BinaryConstraint, UnaryConstraint,CardinalityConstraint, MutexConstraint, ImplicationConstraint
 from src.SecurityDecorator import secured
 
