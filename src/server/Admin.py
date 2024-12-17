e44076b2afc11e0e391ee8e2b3e0415496f35506
ss550 <165291559+ss550@users.noreply.github.com>
2024-12-17 16:07:49 +0100

    Merge

--- src/server/Admin.py
+++ src/server/Admin.py
@@ -1,17 +1,17 @@
-from server.db.UserMapper import UserMapper
-from server.db.WardrobeMapper import WardrobeMapper
-from server.db.ClothingItemMapper import ClothingItemMapper
-from server.db.ClothingTypeMapper import ClothingTypeMapper
-from server.db.StyleMapper import StyleMapper
-from server.db.OutfitMapper import OutfitMapper
-from server.db.ConstraintMapper import ConstraintMapper
-from server.bo.User import User
-from server.bo.Wardrobe import Wardrobe
-from server.bo.ClothingItem import ClothingItem
-from server.bo.ClothingType import ClothingType
-from server.bo.Style import Style
-from server.bo.Outfit import Outfit
-from server.bo.Constraints import Constraints, BinaryConstraint, UnaryConstraint, CardinalityConstraint, MutexConstraint, ImplicationConstraint
+from src.server.db.UserMapper import UserMapper
+from src.server.db.WardrobeMapper import WardrobeMapper
+from src.server.db.ClothingItemMapper import ClothingItemMapper
+from src.server.db.ClothingTypeMapper import ClothingTypeMapper
+from src.server.db.StyleMapper import StyleMapper
+from src.server.db.OutfitMapper import OutfitMapper
+from src.server.db.ConstraintMapper import ConstraintMapper
+from src.server.bo.User import User
+from src.server.bo.Wardrobe import Wardrobe
+from src.server.bo.ClothingItem import ClothingItem
+from src.server.bo.ClothingType import ClothingType
+from src.server.bo.Style import Style
+from src.server.bo.Outfit import Outfit
+from src.server.bo.Constraints import Constraints, BinaryConstraint, UnaryConstraint, CardinalityConstraint, MutexConstraint, ImplicationConstraint
 
 
 class Administration(object):
