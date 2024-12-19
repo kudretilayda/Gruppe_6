875763069b81098cf80b794bb98589903be0f8e7
MorenoContieri <mc076@hdm-stuttgart.de>
2024-12-19 16:29:51 +0100

    update mutex constraint

--- src/server/bo/mutexconstraint.py
+++ src/server/bo/mutexconstraint.py
@@ -1,8 +1,7 @@
 class Kleidungsstueck:
-    def __init__(self,name,item1,item2):
+    def __init__(self,name):
         self.name = name
-        self.item1 = item1
-        self.item2 = item2
+
 
 class Outfit:
     def __init__(self):
@@ -16,9 +15,41 @@ class Outfit:
 
 
 class MutexConstraint:
-    def __init__(self, excluded_pairs):
-        self.excluded_pairs = excluded_pairs
+    def __init__(self):
+        self.item_1 = None
+        self.item_2 = None                          """auschließende paare"""
 
     def validate(self, outfit):
+        for item in outfit.items:
+            if item_1.clothing_type == item_2.clothing_type:
+                print(f'Regel verletzt: {item_1} und {item_2} können nicht gleichzeitig im outfit sein.')
+                return False
+            else:
+                return True
+
+            """if item1 in outfit.get_items() and item2 in outfit.get_items():
+                print(f'Regel verletzt: {item1.name} und {item2.name} können nicht gleichzeitig im outfit sein.')
+                return False
+            else:
+                return True"""
+
+shorts = Kleidungsstueck("shorts")
+anzugshose = Kleidungsstueck("anzugshose")
+tshirt = Kleidungsstueck("tshirt")
+
+outfit1 = Outfit()
+outfit1.add_item(shorts)
+outfit1.add_item(tshirt)
+
+outfit2 = Outfit()
+outfit2.add_item(shorts)
+outfit2.add_item(anzugshose)
+
+constraint = MutexConstraint(excluded_pairs=shorts, anzugshose)
+
+print(constraint.validate(outfit1))
+
+
+
 
 
