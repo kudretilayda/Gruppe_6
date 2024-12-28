7d9f0b40fe21852c7bfbd5fe0c1916a1b131e6c6
Kudret Kilic <kk179@hdm-stuttgart.de>
2024-12-28 17:33:46 +0100

    Update Outfitcreator

--- frontend/src/components/outfits/OutfitCreator.js
+++ frontend/src/components/outfits/OutfitCreator.js
@@ -76,4 +76,62 @@ const OutfitCreator = () => {
         </FormControl>
       )}
 
-      
+      {/* BH-Größe - Nur für Unterwäsche */}
+      {newClothing.type.toLowerCase().includes('underwear') && (
+        <>
+          <FormControl fullWidth margin="normal">
+            <InputLabel>Körbchengröße</InputLabel>
+            <Select
+              label="Körbchengröße"
+              value={newClothing.cupSize}
+              onChange={(e) => setNewClothing({ ...newClothing, cupSize: e.target.value })}
+            >
+              {cupSizes.map((cup) => (
+                <MenuItem key={cup} value={cup}>
+                  {cup}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl fullWidth margin="normal">
+            <InputLabel>Bandgröße</InputLabel>
+            <Select
+              label="Bandgröße"
+              value={newClothing.bandSize}
+              onChange={(e) => setNewClothing({ ...newClothing, bandSize: e.target.value })}
+            >
+              {bandSizes.map((size) => (
+                <MenuItem key={size} value={size}>
+                  {size}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </>
+      )}
+
+      {/* Farbe */}
+      <FormControl fullWidth margin="normal">
+        <InputLabel>Farbe</InputLabel>
+        <Select
+          label="Farbe"
+          value={newClothing.color}
+          onChange={(e) => setNewClothing({ ...newClothing, color: e.target.value })}
+        >
+          {colors.map((color) => (
+            <MenuItem key={color} value={color}>
+              {color}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+
+      <Button type="submit" variant="contained" color="primary">
+        Outfit erstellen
+      </Button>
+    </form>
+  );
+};
+
+export default OutfitCreator;
