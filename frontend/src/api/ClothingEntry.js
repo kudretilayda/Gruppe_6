07d7e1447a61be22a76f407ce3764970e4f2cf90
Kudret Kilic <kk179@hdm-stuttgart.de>
2025-01-02 16:25:41 +0100

    Update Entry/List

---
+++ frontend/src/api/ClothingEntry.js
@@ -0,0 +1,93 @@
+import BusinessObject from "./BusinessObject.js";
+
+// Die Klasse ClothingEntry erbt von der BusinessObject-Klasse.
+// Diese Klasse repräsentiert einen Eintrag für Kleidung mit Bezeichnung, Menge, Einheit, Farbe, Größe und Marke.
+export default class ClothingEntry extends BusinessObject {
+    // Der Konstruktor initialisiert eine neue Instanz der ClothingEntry-Klasse mit den übergebenen Parametern.
+    constructor(designation, quantity, unit, color, size, brand) {
+        super(); // Ruft den Konstruktor der übergeordneten Klasse (BusinessObject) auf.
+        this.designation = designation; // Bezeichnung der Kleidung (z.B. T-Shirt, Hose).
+        this.quantity = quantity; // Menge der Kleidungsstücke.
+        this.unit = unit; // Einheit der Menge (z.B. Stück, Paar).
+        this.color = color; // Farbe der Kleidung.
+        this.size = size; // Größe der Kleidung (z.B. S, M, L, XL).
+        this.brand = brand; // Marke der Kleidung.
+    }
+
+    // Methode zum Setzen der Bezeichnung der Kleidung.
+    setDesignation(designation) {
+        this.designation = designation;
+    }
+
+    // Methode zum Abrufen der Bezeichnung der Kleidung.
+    getDesignation() {
+        return this.designation;
+    }
+
+    // Methode zum Setzen der Menge der Kleidung.
+    setQuantity(quantity) {
+        this.quantity = quantity;
+    }
+
+    // Methode zum Abrufen der Menge der Kleidung.
+    getQuantity() {
+        return this.quantity;
+    }
+
+    // Methode zum Setzen der Einheit der Menge.
+    setUnit(unit) {
+        this.unit = unit;
+    }
+
+    // Methode zum Abrufen der Einheit der Menge.
+    getUnit() {
+        return this.unit;
+    }
+
+    // Methode zum Setzen der Farbe der Kleidung.
+    setColor(color) {
+        this.color = color;
+    }
+
+    // Methode zum Abrufen der Farbe der Kleidung.
+    getColor() {
+        return this.color;
+    }
+
+    // Methode zum Setzen der Größe der Kleidung.
+    setSize(size) {
+        this.size = size;
+    }
+
+    // Methode zum Abrufen der Größe der Kleidung.
+    getSize() {
+        return this.size;
+    }
+
+    // Methode zum Setzen der Marke der Kleidung.
+    setBrand(brand) {
+        this.brand = brand;
+    }
+
+    // Methode zum Abrufen der Marke der Kleidung.
+    getBrand() {
+        return this.brand;
+    }
+
+    // Statische Methode zum Erzeugen von ClothingEntry-Instanzen aus einem JSON-Array.
+    static fromJSON(clothingEntries) {
+        return clothingEntries.map(f => {
+            // Erzeugt eine neue Instanz von ClothingEntry basierend auf den JSON-Daten.
+            let clothingEntry = new ClothingEntry(
+                f.designation,
+                f.quantity,
+                f.unit,
+                f.color,
+                f.size,
+                f.brand
+            );
+            clothingEntry.setId(f.id);  // ID direkt setzen
+            return clothingEntry;
+        });
+    }
+}
---
+++ frontend/src/components/WardrobeEntryList.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Grid } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import DigitalWardrobeAPI from '../API/DigitalWardrobeAPI'; // API für den Kleiderschrank
+import WardrobeEntryForm from './dialogs/WardrobeEntryForm'; // Form zum Hinzufügen/Bearbeiten von Kleidungsstücken
+import ErrorMessage from './dialogs/ErrorMessage'; // Fehlermeldung
+import LoadingSpinner from './dialogs/LoadingSpinner'; // Ladeindikator
+import WardrobeEntryBO from '../API/WardrobeEntryBO'; // Business-Objekt für Kleiderschrank-Einträge
+import { getAuth } from 'firebase/auth';
+import WardrobeEntryCard from './layout/WardrobeEntryCard';
+import DigitalWardrobeAPI from "../api/DigitalWardrobeAPI"; // Layout für die Anzeige von Kleidungsstücken
+
+/**Übersicht aller Kleiderschrank-Einträge */
+
+const WardrobeEntryList = () => {
+    const [wardrobeEntries, setWardrobeEntries] = useState([]);
+    const [showAddForm, setShowAddForm] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [editEntry, setEditEntry] = useState(null);
+
+    useEffect(() => {
+        fetchWardrobeEntries();
+    }, []);
+
+    //Läd alle Kleiderschrank-Einträge aus der DB
+    const fetchWardrobeEntries = async () => {
+        const auth = getAuth();
+        const user = auth.currentUser;
+        const wardrobe_id = await DigitalWardrobeAPI.getAPI().getWardrobeIdByGoogleUserId(user.uid);
+        setLoading(true);
+        try {
+            const entries = await DigitalWardrobeAPI.getAPI().getWardrobeEntriesByWardrobeId(wardrobe_id.wardrobe_id);
+            const wardrobeEntryBOs = WardrobeEntryBO.fromJSON(entries);
+            setWardrobeEntries(wardrobeEntryBOs);
+            setLoading(false);
+        } catch (error) {
+            console.error("Failed to fetch wardrobe entries:", error);
+            setError(error);
+            setLoading(false);
+        }
+    };
+
+    const handleAddButtonClick = () => {
+        setShowAddForm(true);
+        setEditEntry(null);
+    };
+
+    const handleFormClose = (newEntry) => {
+        if (newEntry) {
+            fetchWardrobeEntries();
+        }
+        setShowAddForm(false);
+    };
+
+    const handleEditButtonClick = (entry) => {
+        setShowAddForm(true);
+        setEditEntry(entry);
+    };
+
+    const handleDeleteButtonClick = async (itemName) => {
+        try {
+            await DigitalWardrobeAPI.getAPI().deleteWardrobeEntry(itemName);
+            fetchWardrobeEntries();
+        } catch (error) {
+            console.error("Failed to delete wardrobe entry:", error);
+            setError(`Failed to delete entry: ${itemName}`);
+        }
+    };
+
+    if (loading) {
+        return <LoadingSpinner show={true} />;
+    }
+
+    if (error) {
+        return <ErrorMessage error={error} contextErrorMsg="Failed to load wardrobe entries." />;
+    }
+
+    return (
+        <Grid container spacing={2} style={{ padding: 20 }}>
+            <Grid item xs={12} style={{ textAlign: 'center' }}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    startIcon={<AddIcon />}
+                    onClick={handleAddButtonClick}
+                    sx={{ width: '200px', height: '50px' }}
+                >
+                    Add Clothing
+                </Button>
+            </Grid>
+            {wardrobeEntries.map((entry) => (
+                <Grid item xs={12} sm={6} md={4} key={entry.getId()}>
+                    <WardrobeEntryCard
+                        entry={entry}
+                        onEdit={handleEditButtonClick}
+                        onDelete={handleDeleteButtonClick}
+                    />
+                </Grid>
+            ))}
+            {showAddForm && (
+                <WardrobeEntryForm
+                    show={showAddForm}
+                    wardrobeentry={editEntry}
+                    onClose={handleFormClose}
+                />
+            )}
+        </Grid>
+    );
+};
+
+export default WardrobeEntryList;
