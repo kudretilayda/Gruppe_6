9c3a4f21025fe2abd2d0194426548326d2c94a6d
Aliaksei Lakhmakou <alex.hooke@web.de>
2025-01-09 15:02:27 +0100

    Update CORS Einstellungen

--- src/main.py
+++ src/main.py
@@ -42,6 +42,16 @@ CORS(app, resources={
         "supports_credentials": True
     }
 })
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Cross-Origin-Opener-Policy', 'same-origin')
+    response.headers.add('Cross-Origin-Embedder-Policy', 'require-corp')
+    return response
+
 #CORS(app, resources=r'/wardrobe/*')
 
 # API für Datenstruktur
