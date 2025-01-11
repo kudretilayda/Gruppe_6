f7af65c4d360a1cc794b5388e726acd1227655c4
ss550 <165291559+ss550@users.noreply.github.com>
2025-01-11 22:36:32 +0100

    Implication Constraint
    Implication aus unserer Draft.

---
+++ src/server/bo/Constraints/Implication.py
@@ -0,0 +1,25 @@
+from src.server.bo.Constraints.Constraint import Constraint
+
+
+class ImplicationConstraint(Constraint):
+    def __init__(self, if_type, then_type):
+        super().__init__()
+        self.if_type = if_type
+        self.then_type = then_type
+
+    def validate(self, outfit):
+        has_if_type = False
+        has_then_type = False
+
+        for item in outfit._item:   # Access to a protected member _item of a class
+            if item.clothing_type == self.if_type:
+                has_if_type = True
+            if item.clothing_type == self.then_type:
+                has_then_type = True
+
+        if has_if_type and not has_then_type:
+            print(f"Regel verletzt: Wenn {self.if_type.name} vorhanden ist, "
+                  f"muss auch {self.then_type.name} vorhanden sein.")
+            return False
+        else:
+            return True
