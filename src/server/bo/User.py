f47c50e73ac49906c65da23b98064533c574f555
MorenoContieri <mc076@hdm-stuttgart.de>
2025-01-12 03:36:42 +0100

    User.py
    User-Klasse, die Benutzerdaten wie Vorname, Nachname, Nickname und E-Mail speichert und Methoden bereitstellt, um diese Daten zu setzen und abzurufen

---
+++ src/server/bo/User.py
@@ -0,0 +1,57 @@
+from src.server.bo.BusinessObject import BusinessObject
+
+
+class User (BusinessObject): # Die Klasse User repräsentiert einen Benutzer und erbt von BusinessObject
+
+    def __init__(self):
+        super().__init__()
+        self._user_id = 0
+        self._lastname = ""
+        self._firstname = ""
+        self._nickname = ""
+        self._google_id = ""
+        self._email = ""
+
+    def get_user_id(self):  # Getter und Setter für die Benutzer-ID
+        return self._user_id
+
+    def set_user_id(self, value):
+        self._user_id = value
+
+    def get_lastname(self): # Getter und Setter für den Nachnamen
+        return self._lastname
+
+    def set_lastname(self, lastname):
+        self._lastname = lastname
+
+    def get_firstname(self): # Getter und Setter für den Vornamen
+        return self._firstname
+
+    def set_firstname(self, firstname):
+        self._firstname = firstname
+
+    def get_nickname(self):  # Getter und Setter für den Nickname
+        return self._nickname
+
+    def set_nickname(self, value): 
+        self._nickname = value
+
+    def get_google_id(self): # Getter und Setter für die Google-ID
+        return self._google_id
+
+    def set_google_id(self, value):
+        self._google_id = value
+
+    def get_email(self): # Getter und Setter für die E-Mail-Adresse
+        return self._email
+
+    def set_email(self, value):
+        self._email = value
+
+    def __str__(self):
+        return "User: {}, {}, {}, {}".format(
+            self.get_user_id(),
+            self.get_lastname(),
+            self.get_email(),
+            self.get_firstname()
+        )
