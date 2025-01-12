813a06b302f2b7b05902b058fa6a7d95d2d06c3b
MorenoContieri <mc076@hdm-stuttgart.de>
2025-01-12 03:54:46 +0100

    update Mapper.py
    Die Mapper-Klasse verwaltet die Verbindung zur Datenbank

--- src/server/db/Mapper.py
+++ src/server/db/Mapper.py
@@ -1,8 +1,27 @@
+import mysql.connector as connector
+import os
+from contextlib import AbstractContextManager
 from abc import ABC, abstractmethod
 
-class Mapper(ABC):
+class Mapper(AbstractContextManager,ABC):
     def __init__(self):
-        pass
+        self._cnx = None
+
+    def __enter__(self):
+
+        if os.getenv('DATABASE_URL', '').startswith(''):
+            self._cnx = connector.connect(user='demo', password='demo',
+                                          unix_socket='/cloudsql/digital-wardrobe-442615',
+                                          database='digital_wardrobe')
+
+        else:
+            self._cnx = connector.connect(user='demo', password='demo',
+                                          host='localhost:3306',
+                                          database='digital_wardrobe')
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._cnx.close()
 
     @abstractmethod
     def find_all(self):
